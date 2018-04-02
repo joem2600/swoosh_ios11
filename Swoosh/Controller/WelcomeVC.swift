bb2b74e54031ad5fa5bd196e5c7261f6c0a1bb90
Joe M <joem2600@gmail.com>
2018-04-01 23:37:10 -0400

    lesson 07 - cleaned up code in WelcomeVC & deleted two references

--- Swoosh/Controller/WelcomeVC.swift
+++ Swoosh/Controller/WelcomeVC.swift
@@ -10,22 +10,10 @@ import UIKit
 
 class WelcomeVC: UIViewController {
 
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let w = view.frame.size.width
-//        let h = view.frame.size.height
-//
-//        let wSwoosh = swoosh.frame.size.width
-//        let hSwoosh = swoosh.frame.size.height
-//
-//        swoosh.frame = CGRect(x: w/2 - wSwoosh/2, y: 50, width: wSwoosh, height: hSwoosh)
-//
-//        bgImg.frame = view.frame
+        
     }
 
     @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
